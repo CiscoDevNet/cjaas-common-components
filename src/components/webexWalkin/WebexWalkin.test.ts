242b0c82db7e1466c62744bb340e03180ff1ec63
Elena Giere <egiere@cisco.com>
2021-03-11 11:32:26 -0800

    lowercase component directory name

---
+++ src/components/webexWalkin/WebexWalkin.test.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) Cisco Systems, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { fixture, fixtureCleanup, html } from "@/components/webexWalkin/node_modules/@open-wc/testing-helpers";
+import "./WebexWalkin";
+import { WebexWalkin } from "./WebexWalkin";
+
+describe("WalkIn component", () => {
+  afterEach(() => {
+    fixtureCleanup();
+  });
+
+  test("should render one WalkIn component", async () => {
+    expect.hasAssertions();
+    const component: WebexWalkin.ELEMENT = await fixture(
+      html`
+        <cjaas-webex-walkin></cjaas-webex-walkin>
+      `
+    );
+    expect(component).not.toBeNull();
+  });
+});
