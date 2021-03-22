e3a1621d8bedd770a4223587c3b4ee2293688372
Elena Giere <egiere@cisco.com>
2021-03-22 15:17:25 -0700

    removed all apis within timeline component

--- src/[sandbox]/examples/timeline.ts
+++ src/[sandbox]/examples/timeline.ts
@@ -6,16 +6,5 @@ import { timelineEventsMock } from "../sandbox.mock";
 
 export const timelineTemplate = html`
   <h3>Default</h3>
-  <!-- <cjaas-timeline
-    id="cjaas-timeline"
-    type="journey-and-stream"
-    stream-id="st=demostore&so=sandbox&ss=datasink&sp=w&se=2021-04-23T18:25:43.511Z&sk=sandbox&sig=TEUtd3qKp6pYjoTM7GEHDZeKRfnIWr90MQoW6r2xsB0="
-    pagination="$top=15"
-    render-as="bullets"
-    Limit="15"
-  >
-  </cjaas-timeline> -->
-
-  <cjaas-above-timeline></cjaas-above-timeline>
-  <!-- <cjaas-timeline limit="15" type="journey-and-stream" .timelineEvents=${timelineEventsMock}></cjaas-timeline> -->
+  <cjaas-timeline limit="3" .timelineEvents=${timelineEventsMock}></cjaas-timeline>
 `;
--- src/components/timeline/Timeline.ts
+++ src/components/timeline/Timeline.ts
@@ -6,10 +6,10 @@
  *
  */
 
-import { LitElement, html, property, internalProperty, PropertyValues } from "lit-element";
+import { LitElement, html, property, internalProperty } from "lit-element";
 import { groupBy } from "lodash";
 
-import { getRelativeDate, getTimelineEventFromMessage } from "./utils";
+import { getRelativeDate } from "./utils";
 import { repeat } from "lit-html/directives/repeat";
 import { customElementWithCheck } from "@/mixins";
 import "../timeline/TimelineItem";
@@ -42,175 +42,8 @@ export namespace Timeline {
   export class ELEMENT extends LitElement {
     @property({ type: Array }) timelineEvents: TimelineEvent[] = [];
     @property({ type: Number }) limit = 5;
-    @property({ reflect: true }) type: | "journey" | "livestream" | "journey-and-stream" = "livestream";
 
     @internalProperty() expandDetails = false;
-
-    // eventSource: EventSource | null = null;
-
-    connectedCallback() {
-      super.connectedCallback();
-      this.setupEvents();
-    }
-
-    disconnectedCallback() {
-      super.disconnectedCallback();
-      this.teardownEvents();
-    }
-
-    private setupEvents() {
-      this.addEventListener("enqueue-event", this.enqueueEvent as EventListener);
-    }
-
-    private teardownEvents() {
-      this.removeEventListener("enqueue-event", this.enqueueEvent as EventListener);
-    }
-
-    updated(changedProperties: PropertyValues) {
-      super.updated(changedProperties);
-
-      // if (changedProperties.has('timelineEvents')) {
-      //   console.log('[log][timeline] timelineEvents', this.timelineEvents);
-      // }
-      // let flag = false;
-      // changedProperties.forEach((oldValue: string, name: string) => {
-      //   console.log("Oldvalue", oldValue);
-      //   // uses camelcase
-      //   if (name === "streamId" && this.streamId) {
-      //     flag = true;
-      //   } else if (
-      //     name === "filter" &&
-      //     this.filter !== oldValue &&
-      //     this.streamId
-      //   ) {
-      //     flag = true;
-      //   }
-      // });
-
-      // if (flag) {
-      //   this.timelineEvents = [];
-      //   this.requestUpdate();
-      //   this.subscribeToStream();
-      // }
-    }
-
-    private notifyEnqueueEvent(detail: TimelineEvent) {
-      this.dispatchEvent(
-        new CustomEvent<TimelineEvent>("enqueue-event", {
-          composed: true,
-          bubbles: true,
-          detail
-        })
-      );
-    }
-
-    // public enqueueEvent(event: TimelineEvent) {
-    public enqueueEvent(event: CustomEvent<{ event: TimelineEvent }>) {
-      const { event: enqueuedEvent } = event.detail;
-      // logger.info("adding new event");
-
-      while (
-        this.timelineEvents.length >= this.limit &&
-        this.type === "livestream"
-      ) {
-        this.dequeuePastOneEvent();
-      }
-
-      const dataLength = this.timelineEvents.length;
-
-      // events may not be chronologically sorted by default
-      if (dataLength === 0) {
-        this.timelineEvents = [enqueuedEvent];
-      } else if (this.timelineEvents[0].timestamp < enqueuedEvent.timestamp) {
-        this.timelineEvents = [enqueuedEvent, ...this.timelineEvents];
-      } else if (this.timelineEvents[dataLength - 1].timestamp > enqueuedEvent.timestamp) {
-        this.timelineEvents = [...this.timelineEvents, enqueuedEvent];
-      } else {
-        let currentIndex = 0;
-        let currentItem = this.timelineEvents[currentIndex];
-        while (
-          currentItem.timestamp > enqueuedEvent.timestamp &&
-          currentIndex < this.timelineEvents.length
-        ) {
-          currentIndex = currentIndex + 1;
-          currentItem = this.timelineEvents[currentIndex];
-        }
-        this.timelineEvents.splice(currentIndex, 0, enqueuedEvent);
-      }
-    }
-
-    dequeuePastOneEvent() {
-      this.timelineEvents.shift();
-    }
-
-    // defaults to top 10 for journey
-    // getAPIQueryParams(forJourney = false) {
-    //   let url = this.streamId;
-    //   if (this.filter) {
-    //     url += `&$filter=${this.filter}`;
-    //   }
-
-    //   if (this.pagination) {
-    //     url += `&${this.pagination}`;
-    //   } else if (!this.pagination && forJourney) {
-    //     url += "&$top=10";
-    //   }
-    //   console.log('[log] getAPIQueryParams', url);
-    //   return url;
-    // }
-
-    // getJourney() {
-    //   // gets historic journey
-    //   console.log('[log] getJourney here1', `${this.baseURL}/journey?${this.getAPIQueryParams(true)}`);
-    //   fetch(`${this.baseURL}/journey?${this.getAPIQueryParams(true)}`, {
-    //     headers: {
-    //       "content-type": "application/json; charset=UTF-8",
-    //     },
-    //     method: "GET",
-    //   })
-    //     .then((x: Response) => x.json())
-    //     .then((x: Array<ServerSentEvent>) => {
-    //       console.log('[log] getJourney here2', x);
-    //       x?.map((y: ServerSentEvent) => {
-    //         console.log('[log] getJourney here3', y);
-    //         return getTimelineEventFromMessage(y);
-    //       }
-    //       ).map((z: TimelineEvent) => this.enqueueEvent(z));
-    //     });
-    // }
-
-    // subscribeToStream() {
-    //   if (this.eventSource) {
-    //     this.eventSource.close();
-    //   }
-
-    //   if (this.type === "journey" || this.type === "journey-and-stream") {
-    //     this.getJourney();
-    //   }
-
-    //   if (this.type !== "journey") {
-    //     this.eventSource = new EventSource(
-    //       `${this.baseURL}/real-time?${this.getAPIQueryParams()}`
-    //     );
-
-    //     this.eventSource.onmessage = (event: ServerSentEvent) => {
-    //       let data;
-    //       try {
-    //         data = JSON.parse(event.data);
-    //       } catch (err) {
-    //         // received just the timestamp
-    //       }
-
-    //       if (data) {
-    //         this.enqueueEvent(getTimelineEventFromMessage(data));
-    //       }
-    //     };
-
-    //     this.eventSource.onerror = () => {
-    //       this.subscribeToStream();
-    //     };
-    //   }
-    // }
 
     toggleDetailView = () => {
       this.expandDetails = !this.expandDetails;
@@ -218,14 +51,8 @@ export namespace Timeline {
 
     renderDetailsControl = () => {
       return html`
-        <md-button
-          class="collapse-details"
-          hasRemoveStyle
-          @click="${this.toggleDetailView}"
-        >
-          ${this.expandDetails
-            ? "Collapse All Details"
-            : "Expand All Details"}</md-button
+        <md-button class="collapse-details" hasRemoveStyle @click="${this.toggleDetailView}">
+          ${this.expandDetails ? "Collapse All Details" : "Expand All Details"}</md-button
         >
       `;
     };
@@ -239,17 +66,21 @@ export namespace Timeline {
           ${repeat(
             groupedEvent.children,
             (event: TimelineEvent) => event.id,
-            (event: TimelineEvent) => html`
-              <cjaas-timeline-item
-                .title=${event.title}
-                .timestamp=${event.timestamp}
-                .data=${event.data}
-                .id=${event.id}
-                .person=${event.person || null}
-                ?expanded="${this.expandDetails}"
-                class="has-line"
-              ></cjaas-timeline-item>
-            `
+            (event: TimelineEvent, currentIndex: number) => {
+              if (currentIndex < this.limit) {
+                return html`
+                  <cjaas-timeline-item
+                    .title=${event.title}
+                    .timestamp=${event.timestamp}
+                    .data=${event.data}
+                    .id=${event.id}
+                    .person=${event.person || null}
+                    ?expanded="${this.expandDetails}"
+                    class="has-line"
+                  ></cjaas-timeline-item>
+                `;
+              }
+            }
           )}
         </div>
       `;
@@ -261,9 +92,7 @@ export namespace Timeline {
 
     render() {
       // groups events by date
-      const groupedDates = groupBy(this.timelineEvents, (event: TimelineEvent) =>
-        getRelativeDate(event.timestamp)
-      );
+      const groupedDates = groupBy(this.timelineEvents, (event: TimelineEvent) => getRelativeDate(event.timestamp));
 
       const groupedEvents = Object.keys(groupedDates).map((key: string) => {
         const obj = { key, children: groupedDates[key] };
@@ -280,8 +109,8 @@ export namespace Timeline {
             <div class="stream">
               ${repeat(
                 groupedEvents,
-                (eventData) => eventData.key,
-                (eventData) => this.renderEventItems(eventData)
+                eventData => eventData.key,
+                eventData => this.renderEventItems(eventData)
               )}
             </div>
             <div class="footer"></div>
