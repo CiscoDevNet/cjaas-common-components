3ed8523d352a7505332b8fa887ec0d0e1ddfb6a8
Kevin Hyde <kehyde@cisco.com>
2021-08-19 13:44:44 -0700

    Enable core deployment of component.

--- src/components/event-toggles/EventToggles.ts
+++ src/components/event-toggles/EventToggles.ts
@@ -7,11 +7,49 @@ import { customElementWithCheck } from "@/mixins";
 export namespace EventToggles {
   @customElementWithCheck("cjaas-event-toggles")
   export class ELEMENT extends LitElement {
-    @property({}) test = null;
+    @property({ type: Array, attribute: false }) eventTypes = [];
+    @property({ type: Array, attribute: false }) activeTypes = ["base"];
+
+    toggleFilter(type: string, e: Event) {
+      if (this.activeTypes && this.activeTypes.includes(type)) {
+        this.activeTypes = this.activeTypes.filter(item => item !== type);
+      } else {
+        this.activeTypes.push(type);
+      }
+
+      (e.target! as HTMLElement).blur();
+      this.requestUpdate();
+    }
+
+    checkFilter(type: string) {
+      return this.activeTypes.includes(type);
+    }
+
+    renderFilterButtons() {
+      return this.eventTypes.map(item => {
+        return html`
+          <md-button
+            id="filter-${item}"
+            ?active=${this.checkFilter(item)}
+            outline
+            color="blue"
+            size="28"
+            @click=${(e: Event) => this.toggleFilter(item, e)}
+            >${item}</md-button
+          >
+        `;
+      });
+    }
+
+    static get styles() {
+      return [styles];
+    }
 
     render() {
       return html`
-        <div></div>
+        <div class="filter-buttons">
+          ${this.renderFilterButtons()}
+        </div>
       `;
     }
   }
