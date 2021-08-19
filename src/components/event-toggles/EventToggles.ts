2b1600ceeebeddd0f383cdb988834c304ca02343
Kevin Hyde <kehyde@cisco.com>
2021-08-19 14:16:13 -0700

    Add docs, connect custom event emitter

--- src/components/event-toggles/EventToggles.ts
+++ src/components/event-toggles/EventToggles.ts
@@ -1,14 +1,24 @@
 import { LitElement, html, property } from "lit-element";
-import { nothing } from "lit-html";
-import { DateTime } from "luxon";
 import styles from "./scss/module.scss";
 import { customElementWithCheck } from "@/mixins";
+
+/*
+Event Toggles Component
+Used by Timeline based components to dynamically render filter toggles for the types of events in an event Stream.
+INPUTS:
+.eventTypes: Array<string>
+.activeTypes: Array<string>
+OUTPUT:
+@active-type-update custom event: Array<string>
+Pass in the eventTypes and activeTypes fetched in the consuming widget with the property selector.
+Listen for the @active-type-update custom event in order to reflect and control filtering from the wrapping widget.
+*/
 
 export namespace EventToggles {
   @customElementWithCheck("cjaas-event-toggles")
   export class ELEMENT extends LitElement {
-    @property({ type: Array, attribute: false }) eventTypes = [];
-    @property({ type: Array, attribute: false }) activeTypes = ["base"];
+    @property({ type: Array, attribute: false }) eventTypes: Array<string> = [];
+    @property({ type: Array, attribute: false }) activeTypes: Array<string> = [];
 
     toggleFilter(type: string, e: Event) {
       if (this.activeTypes && this.activeTypes.includes(type)) {
@@ -16,7 +26,15 @@ export namespace EventToggles {
       } else {
         this.activeTypes.push(type);
       }
-
+      this.dispatchEvent(
+        new CustomEvent("active-type-update", {
+          bubbles: true,
+          composed: true,
+          detail: {
+            activeTypes: this.activeTypes
+          }
+        })
+      );
       (e.target! as HTMLElement).blur();
       this.requestUpdate();
     }
