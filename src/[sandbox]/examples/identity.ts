4c93f58126b00cfa430f71d47b8dea4fefddb3e5
Elena Giere <egiere@cisco.com>
2022-10-19 12:07:45 -0700

    Version 8.4.9: alias restructuring changes (#57)
    * alias restructuring with types: Profile & Identity changes
    
    * update identity error message strings
    
    * version 4.3.9: alias Restructuring tasks

--- src/[sandbox]/examples/identity.ts
+++ src/[sandbox]/examples/identity.ts
@@ -13,8 +13,19 @@ const identityData: IdentityData = {
   id: "123-789-1010",
   createdAt: "1656625453851",
   modifiedAt: "1656625453851",
-  aliases: ["Alex", "Ross", "A-rod"],
+  aliases: ["+1 (800) 300-8080", "arod@gmail.com"],
 };
+
+const aliasObjects = [
+  {
+    type: "Email",
+    value: "egiere@gmail.com",
+  },
+  {
+    type: "Unknown",
+    value: "elena",
+  },
+];
 
 const aliasDeleteInProgress = {
   Alex: true,
@@ -22,8 +33,8 @@ const aliasDeleteInProgress = {
 
 export const identityTemplate = html`
   <h3>Default</h3>
-  <cjaas-identity .identityData=${identityData} customer="Alex Ross"></cjaas-identity>
-
+  <cjaas-identity .identityData=${identityData} .aliasObjects=${aliasObjects} customer="Alex Ross"></cjaas-identity>
+  <!-- 
   <h3>No Aliases</h3>
   <cjaas-identity .customer=${"Alex Ross"}> </cjaas-identity>
 
@@ -34,17 +45,12 @@ export const identityTemplate = html`
   <cjaas-identity .customer=${null}> </cjaas-identity>
 
   <h3>Alias Delete In Progress</h3>
-  <cjaas-identity
-    .alias=${alias}
-    .aliasDeleteInProgress=${aliasDeleteInProgress}
-    .aliasAddInProgress=${true}
-    .customer=${"Alex Ross"}
-  >
+  <cjaas-identity .alias=${alias} .aliasDeleteInProgress=${aliasDeleteInProgress} .aliasAddInProgress=${true}>
   </cjaas-identity>
 
   <h3>Alias Add In Progress</h3>
   <cjaas-identity .alias=${alias} .aliasAddInProgress=${true} .customer=${"Alex Ross"}> </cjaas-identity>
 
   <h3>Alias Get In Progress</h3>
-  <cjaas-identity .alias=${alias} aliasGetInProgress .customer=${"Alex Ross"}> </cjaas-identity>
+  <cjaas-identity .alias=${alias} aliasGetInProgress .customer=${"Alex Ross"}> </cjaas-identity> -->
 `;
